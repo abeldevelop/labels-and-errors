5b16ab3db2b171b8d63eb68d25b6404620a0ea6b
Abel Fernandez <abelfergodevelop@gmail.com>
2020-02-01 22:23:05 +0100

    Add enable Feign clients

--- labels-and-errors-service/src/main/java/com/abeldevelop/architecture/service/labelsanderrors/service/MainServiceApplication.java
+++ labels-and-errors-service/src/main/java/com/abeldevelop/architecture/service/labelsanderrors/service/MainServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("${abeldevelop.configuration.entity-scan}")
 @EnableJpaRepositories("${abeldevelop.configuration.enable-jpa-repositories}")
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = {"${abeldevelop.configuration.component-scan}"})
 @SpringBootApplication
 public class MainServiceApplication {
 
