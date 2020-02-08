300498e441609b793071eafc198b5bd91eccd6f4
Abel Fernandez <abelfergodevelop@gmail.com>
2020-02-08 14:48:53 +0100

    Fix metamodel

--- labels-and-errors-model/src/main/java/com/abeldevelop/architecture/service/labelsanderrors/model/ErrorMessageEntity_.java
+++ labels-and-errors-model/src/main/java/com/abeldevelop/architecture/service/labelsanderrors/model/ErrorMessageEntity_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 @StaticMetamodel(ErrorMessageEntity.class)
 public class ErrorMessageEntity_ {
 
-	public static volatile SingularAttribute<ErrorMessageEntity, String> id;
+	public static volatile SingularAttribute<ErrorMessageEntity, Long> id;
 	public static volatile SingularAttribute<ErrorMessageEntity, String> serviceName;
 	public static volatile SingularAttribute<ErrorMessageEntity, String> languageCode;
 	public static volatile SingularAttribute<ErrorMessageEntity, String> code;
